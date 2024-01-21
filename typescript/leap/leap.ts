d283929186a50d2e5ccb43797542d168a886adb7
Adrian HM <adriandev@Adrians-MacBook-Pro.local>
2024-01-21 17:48:02 +0100

    leap year exercise resolved

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,11 @@
-export function isLeap() {
-  throw new Error('Remove this statement and implement this function')
+function isDivisible(year: number, number: number) {
+  return year % number === 0;
+}
+
+export function isLeap(year: number) {
+  if (!isDivisible(year, 4)) return false;
+
+  if (isDivisible(year, 100) && !isDivisible(year, 400)) return false;
+
+  return true;
 }
